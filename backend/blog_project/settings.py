6e53cf80d02f367b45c7db5602d4b732e3fd78b0
fatfatdragon <1310195396@qq.com>
2025-03-05 22:32:49 +0800

    8

--- backend/blog_project/settings.py
+++ backend/blog_project/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog.middleware.CorsMiddleware',
+    #'blog.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
